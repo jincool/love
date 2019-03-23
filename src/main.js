b7dac5f0de91fdecd14e79f74744f1eacbc4202b
jincool <634596685@qq.com>
2019-03-24 01:42:07 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,10 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+//配置axios
+import Axios from 'axios'
 //配置Vant的JS
 import Vant from 'vant'
 //配置Vant的JS
 import 'vant/lib/index.css';
+
+//配置公共请求地址
+Axios.defaults.baseURL='http://127.0.0.1/volservice/?a=getApi';
+Vue.prototype.$axios=Axios ;
 
 //注册Vant全局组建及挂在
 Vue.use(Vant);
