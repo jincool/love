74d2611ca57c882cc875348273707adb0da22e79
jincool <634596685@qq.com>
2019-04-01 13:43:08 +0800

    添加顶部标题栏组件

---
+++ src/components/common/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <van-nav-bar
+            title=""
+            fixed
+    >
+        <div slot="title">
+            <slot name="title"></slot>
+        </div>
+        <slot name="edit"></slot>
+    </van-nav-bar>
+</template>
+
+<script>
+    export default {
+        name: "header"
+    }
+</script>
+
+<style scoped>
+    .van-nav-bar {
+        background-color: #FFC0CB;
+    }
+
+    .van-nav-bar__title {
+        color: #fffbff;
+    }
+</style>
