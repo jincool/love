4e245e892fbe8988c27cbf4d5be20c90bf77853b
jincool <634596685@qq.com>
2019-04-16 11:20:47 +0800

    退出按钮

--- src/components/Setting/Setting.vue
+++ src/components/Setting/Setting.vue
@@ -1,10 +1,21 @@
 <template>
-   <div>我是设置</div>
+   <div>
+      <van-button @click="logOut" type="warning">退出</van-button>
+   </div>
 </template>
 
 <script>
     export default {
-        name: "Setting"
+        name: "Setting",
+        methods:{
+            logOut() {
+                localStorage.removeItem('token');
+                this.$store.commit('logout');
+                // 移除之前在axios头部设置的token,现在将无法执行需要token的事务
+                delete this.$axios.defaults.headers.common['Authorization'];
+                this.$router.push('/login');
+            }
+        }
 
     }
 </script>
