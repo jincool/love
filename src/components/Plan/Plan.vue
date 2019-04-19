846aed22ad59198a7c41e28d37e1e6a90f7e8a1b
jincool <634596685@qq.com>
2019-04-19 11:23:02 +0800

    Update Plan.vue
    新增计划组建查询

--- src/components/Plan/Plan.vue
+++ src/components/Plan/Plan.vue
@@ -13,7 +13,14 @@
       <br>
       <!--下啦刷新-->
       <van-pull-refresh  v-model="isLoading" @refresh="onRefresh" >
-
+         <van-list
+                 v-model="loading"
+                 :finished="finished"
+                 finished-text="没有更多了"
+                 :offset="30"
+                 @load="onLoad"
+                 ref="check"
+         >
 
          <van-row class="plan_panel" v-for="(info,index) in mainInfo" :key="index" >
             <van-col span="4">
@@ -36,7 +43,7 @@
                      <van-cell value="" :class="detailClass(info.sex)">
                         <div slot="">
                            <van-icon name="underway-o" color="#ff905d" />
-                           {{info.dateInfo}}
+                           {{info.date_info}}
                         </div>
                         <div slot="">
                            <van-icon name="location-o" color="#02cb64" />
@@ -55,7 +62,7 @@
 
 
 
-
+         </van-list>
          <!--<p>刷新次数: {{ count }}</p>-->
       </van-pull-refresh>
 
@@ -72,17 +79,13 @@
         data(){
             return{
                 title:'计划',
-                mainInfo:[
-                    {'content':'计划去青岛玩耍',dateInfo:'2019-05-01',address:'青岛市','sex':1},
-                    {'content':'计划去西安玩耍',dateInfo:'2019-06-01',address:'青岛市','sex':0},
-                    {'content':'计划去北京玩耍',dateInfo:'2019-07-01',address:'青岛市','sex':1},
-                ],
-                content:'计划去青岛玩耍',//计划内容
-                dateInfo:'2019-05-01',//计划时间
-                address:'青岛市',//计划地点
+                mainInfo:[],
                 count: 0,
                 isLoading: false,
-                imageURL:require('@/assets/img/logo.png')
+                imageURL:require('@/assets/img/logo.png'),
+                page:1,
+                loading: false,//启停加载
+                finished: false,//启停结束加载
             }
         },
 
@@ -105,8 +108,44 @@
             },
             addInfo(){
 
+            },
+            planInfo(page){
+                this.$axios.post('index/Plan/planInfo',{'page':page})
+                    .then(res=>{
+                     this.mainInfo=res.data;
+                        this.page ++;
+                    //console.log(res.data)
+                   //  this.mainInfo.concat(res.data)
+                   // console.log(this.mainInfo);
+                })
+            },
+            // 上拉加载
+            onLoad(){
+                // setTimeout(() => {
+                this.$axios.post('index/Plan/planInfo',{'page':this.page})
+                    .then(res=>{
+                        // this.mainInfo=res.data;
+                        if (res.data.length === 0) {
+                            this.finished = true;
+                            this.$toast('有底线啦');
+                            this.loading = false;
+                            return false
+                        }
+                        // console.log(this.$refs.check)
+                        this.mainInfo=this.mainInfo.concat(res.data)
+                        this.loading = false;
+                        this.page ++;
+                         // console.log(this.mainInfo);
+                    })
+
+                // }, 500);
             }
+
+        },
+        created(){
+            // this.planInfo(this.page);
         }
+
 
     }
 </script>
